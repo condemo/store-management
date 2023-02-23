d9450570c2ede9d7437fc7f458cac0f1752e9f07
Gustavo de los Santos <gusleo94@gmail.com>
2023-02-23 20:39:11 +0100

    feat: basic product tables added ( stock, discount, brand, category )

--- app/products/products_models.py
+++ app/products/products_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, Float, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, Float, Integer, String
 from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
@@ -17,3 +17,31 @@ class Product(Base):
     created_at = Column(DateTime(timezone=True),
                         nullable=False, default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+class Brand(Base):
+    __tablename__ = "brands"
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+
+
+class ProductCategory(Base):
+    __tablename__ = "products_category"
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+
+
+class Discount(Base):
+    __tablename__ = "discounts"
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    desc = Column(String, nullable=True)
+    discount_percent = Column(Integer, nullable=False, default=0)
+    active = Column(Boolean, nullable=False, default=False)
+    created_at = Column(DateTime(timezone=True), nullable=False,
+                        default=func.now())
+    updated_at = Column(DateTime(timezone=True), nullable=False,
+                        onupdate=func.now())
