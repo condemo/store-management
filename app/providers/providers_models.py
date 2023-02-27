adf85a15070b4ebc94cc1359e04c1c77e4e10067
Gustavo de los Santos <gusleo94@gmail.com>
2023-02-27 19:48:38 +0100

    feat: providers_orders table and relationship with provider table added

--- app/providers/providers_models.py
+++ app/providers/providers_models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, DateTime, Integer, String, func
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, func
+from sqlalchemy.orm import mapped_column, relationship
 from ..database import Base
 
 
@@ -13,3 +14,19 @@ class Provider(Base):
     web_url = Column(String)
     created_at = Column(DateTime(timezone=True), nullable=False, default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    orders = relationship("ProviderOrder", back_populates="provider")
+
+
+class ProviderOrder(Base):
+    __tablename__ = "provider_orders"
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    provider_id = mapped_column(ForeignKey("providers.id", ondelete="CASCADE"), nullable=False)
+    received = Column(Boolean, nullable=False)
+    paid = Column(Boolean, nullable=False)
+    provider_ticket_id = Column(String)
+    created_at = Column(DateTime(timezone=True), nullable=False, default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    provider = relationship("Provider", back_populates="orders")
