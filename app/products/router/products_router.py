25f799ceb780d9e52ff105ffbfa0686f4867d677
Gustavo de los Santos <gusleo94@gmail.com>
2023-03-10 18:32:34 +0100

    feat: product price can be updated

--- app/products/router/products_router.py
+++ app/products/router/products_router.py
@@ -79,21 +79,20 @@ async def create_product(new_product: products_schemas.ProductCreate,
     return product
 
 
-# @router.put("/", response_model=products_schemas.ProductCompleteResponse)
-# async def update_product(updated_product: products_schemas.ProductUpdate,
-#                          db: Session = Depends(get_db)):
-#     product_query = db.query(products_models.Product).filter(
-#             products_models.Product.id == updated_product.id)
-#     product = product_query.first()
-#
-#     if not product:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-#                             detail=f"The product with id {id} is not found")
-#
-#     product_query.update(updated_product.dict(), synchronize_session=False)
-#     db.commit()
-#
-#     return product_query.first()
+@router.put("/update-price", response_model=products_schemas.ProductCompleteResponse)
+async def update_product_price(id: int, price: float,
+                               db: Session = Depends(get_db)):
+    product_query = db.query(products_models.Product) \
+            .filter(products_models.Product.id == id)
+    product = product_query.first()
+
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"The product with id {id} is not found")
+    product_query.update({"price": price})
+    db.commit()
+
+    return product_query.first()
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
