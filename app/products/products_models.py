8fffd296cc255829abadad3c0c2f0acee35b425e
Gustavo de los Santos <gusleo94@gmail.com>
2023-03-13 18:17:12 +0100

    refactor: product name isnt unique anymore

--- app/products/products_models.py
+++ app/products/products_models.py
@@ -11,7 +11,7 @@ class Product(Base):
     __tablename__ = "products"
 
     id = Column(Integer, nullable=False, primary_key=True)
-    name = Column(String(100), nullable=False, unique=True)
+    name = Column(String(100), nullable=False)
     price = Column(Float)
     provider_price = Column(Float)
     category_id = mapped_column(ForeignKey("products_category.id", ondelete="CASCADE"))
